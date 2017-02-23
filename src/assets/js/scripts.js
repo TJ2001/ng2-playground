4c0d6633f48c3318a435b2d2205ee7af1f017d6d
TJ2001 <jung.m.tim@gmail.com>
2017-02-23 14:11:09 -0800

    fix white space issue with sidenav

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,9 +1,26 @@
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginRight = "250px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginRight= "0";
+}
+
+function display() {
+  $("#detail").toggle();
+}
+
+function openClose() {
+    var width = document.getElementById("mySidenav").offsetWidth;
+    console.log(width);
+    if (width == 0){
+      this.openNav();
+    } else {
+      this.closeNav();
+    }
+}
+
+function display(){
+  openClose();
+  display();
 }
