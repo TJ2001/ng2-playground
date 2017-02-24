2f136f6fdf176b993d08375e0ac893555dcf97ec
TJ2001 <jung.m.tim@gmail.com>
2017-02-24 15:43:37 -0800

    prepare for new animation

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -8,7 +8,7 @@ function closeNav() {
 
 function openClose() {
     var width = document.getElementById("mySidenav").offsetWidth;
-    console.log(width);
+    // console.log(width);
     if (width == 0){
       this.openNav();
     } else {
@@ -16,22 +16,69 @@ function openClose() {
     }
 }
 
+
+
 function hideAll(){
-  $("h4").each(function(){
+  $(".recommendation").each(function(){
     $(this).hide();
   });
 }
 
-function displaySlide1(){
+
+function displayDetail1(){
   openNav();
   hideAll();
   $( "#detail" ).show();
+  // setTimeout(function(){$( "#detail" ).show(); }
+  //   , 300);
 }
 
-function displaySlide2(){
+function displayDetail2(){
   openNav();
   hideAll();
-  $( "#detail2" ).show();
+  // $( "#detail" ).show();
+  setTimeout(function(){$( "#detail2" ).show(); }
+    , 300);
+}
+
+function displayDetail3(){
+  openNav();
+  hideAll();
+  // $( "#detail" ).show();
+  setTimeout(function(){$( "#detail3" ).show(); }
+    , 300);
+}
+
+function displayDetail4(){
+  openNav();
+  hideAll();
+  // $( "#detail" ).show();
+  setTimeout(function(){$( "#detail4" ).show(); }
+    , 300);
+}
+
+function displayDetail5(){
+  openNav();
+  hideAll();
+  // $( "#detail" ).show();
+  setTimeout(function(){$( "#detail5" ).show(); }
+    , 300);
+}
+
+function displayDetail6(){
+  openNav();
+  hideAll();
+  // $( "#detail" ).show();
+  setTimeout(function(){$( "#detail6" ).show(); }
+    , 300);
+}
+
+function displayDetail7(){
+  openNav();
+  hideAll();
+  // $( "#detail" ).show();
+  setTimeout(function(){$( "#detail7" ).show(); }
+    , 300);
 }
 
 function displaySlide3(){
